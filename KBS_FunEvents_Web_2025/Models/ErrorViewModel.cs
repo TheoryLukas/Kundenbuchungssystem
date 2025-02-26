98e5544b98f21dde7883267a4ab8e97632c8b582
Lukas Hannawald <lukas.hannawald@proton.me>
2025-02-26 13:15:43 +0100

    initializing project

---
+++ KBS_FunEvents_Web_2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KBS_FunEvents_Web_2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
