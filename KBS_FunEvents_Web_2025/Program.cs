8cef19eb67b899aa8cbe91d03997c1cbf6da35e7
22ita006 <22ita006@BS1O211020>
2025-02-27 10:45:41 +0100

    Set initial configuration

--- KBS_FunEvents_Web_2025/Program.cs
+++ KBS_FunEvents_Web_2025/Program.cs
@@ -1,7 +1,15 @@
+using KBS_Web.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<kbsContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
