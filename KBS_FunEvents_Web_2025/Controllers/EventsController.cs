90b613c6c5db5153d649aac99166180f01728645
Lukas Hannawald <lukas.hannawald@proton.me>
2025-02-27 17:33:20 +0100

    Create Event Page

---
+++ KBS_FunEvents_Web_2025/Controllers/EventsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace KBS_FunEvents_Web_2025.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: EventsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
