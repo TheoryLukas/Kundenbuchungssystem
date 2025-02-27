609ea79fa5c38574207f65cfffa1664e4a608f99
GratlerG <tobiasgratler@gmail.com>
2025-02-27 11:38:10 +0100

    Informations-Seite (Über uns)

---
+++ KBS_FunEvents_Web_2025/Views/Home/Information.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KBS_FunEvents_Web_2025.Views.Home
+{
+    public class InformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
