6d49827f643c2b49a97fb7a7d8db18d377de1d5b
Lukas Hannawald <lukas.hannawald@proton.me>
2025-02-28 17:06:07 +0100

    Create Table for Events

--- KBS_FunEvents_Web_2025/Controllers/EventsController.cs
+++ KBS_FunEvents_Web_2025/Controllers/EventsController.cs
@@ -1,14 +1,25 @@
+using KBS_Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KBS_FunEvents_Web_2025.Controllers
 {
     public class EventsController : Controller
     {
+        private readonly kbsContext _dbContext;
+
+        public EventsController(kbsContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         // GET: EventsController
         public ActionResult Index()
         {
-            return View();
+            List<TblEvVeranstalter> veranstalter = _dbContext.TblEvVeranstalters.ToList();
+            List<TblEvKategorie> kategorien = _dbContext.TblEvKategories.ToList();
+            List<TblEvent> events = _dbContext.TblEvents.ToList();
+            
+            return View("Views/Events/Events.cshtml", events);
         }
-
     }
 }
