523be2fda4309863e3514cd38ceaf168618dde25
22ita008 <maximilian.jodlbauer@msg.group>
2025-02-28 11:23:08 +0100

    ViewModel hinzugefügt

--- KBS_FunEvents_Web_2025/Models/kbsContext.cs
+++ KBS_FunEvents_Web_2025/Models/kbsContext.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using KBS_FunEvents_Web_2025.Models.ViewModels;
 
 namespace KBS_Web.Models;
 
@@ -81,4 +82,6 @@ public partial class kbsContext : DbContext
     }
 
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+public DbSet<KBS_FunEvents_Web_2025.Models.ViewModels.DetailsViewModel> DetailsViewModel { get; set; } = default!;
 }
