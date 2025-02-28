97d07cecb64aaa73cb1fcf5b66e7f96ce2ec0647
22ita009 <benedikt.klingenberger@web.de>
2025-02-28 09:56:11 +0100

    Nur Bene arbeitet hier

---
+++ KBS_FunEvents_Web_2025/Controllers/BuchungController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KBS_FunEvents_Web_2025.Controllers
+{
+    public class BuchungController : Controller
+    {
+        public IActionResult Details()
+        {
+            return View("Details");
+        }
+    }
+}
