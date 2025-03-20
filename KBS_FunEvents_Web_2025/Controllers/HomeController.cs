260a3e8ad741dd8ff39b5588ac4e362b2fdd01a4
22ITA013 <jakob.n@online.de>
2025-03-20 11:59:06 +0100

    Event Buchen zwischenstand

--- KBS_FunEvents_Web_2025/Controllers/HomeController.cs
+++ KBS_FunEvents_Web_2025/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using KBS_FunEvents_Web_2025.Models;
+using KBS_Web.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace KBS_FunEvents_Web_2025.Controllers;
 
@@ -8,9 +10,36 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly kbsContext _dbContext;
+
+    public HomeController(ILogger<HomeController> logger, kbsContext dbContext)
     {
         _logger = logger;
+        this._dbContext = dbContext;
+    }
+
+    private List<TblEvent> GetEvents()
+    {
+        _ = _dbContext.TblEvVeranstalters.ToList();
+        _ = _dbContext.TblEvKategories.ToList();
+
+        List<int> activeEvents = _dbContext.TblEventDatens.ToList()
+                                    .Where(ed => ed.EdFreigegeben == true)
+                                    .DistinctBy(ed => ed.EtEventId)
+        .Select(ed => ed.EtEventId).ToList();
+
+        List<TblEvent> events = _dbContext.TblEvents.ToList()
+                                    .Where(ev => activeEvents.Contains(ev.EtEventId)).ToList();
+
+        return events;
+    }
+
+    private TblEventDaten GetEventDaten(int eventDatenId)
+    {
+
+        TblEventDaten eventDaten = (TblEventDaten)_dbContext.TblEventDatens.Where(ed => ed.EdEvDatenId == eventDatenId);
+
+        return eventDaten;
     }
 
     public IActionResult Index()
@@ -31,6 +60,15 @@ public class HomeController : Controller
     public IActionResult Kontakt()
     {
         return View();
+    }
+
+    public IActionResult NeuesEventFinden()
+    {
+        return View("Views/Home/NeuesEventFinden.cshtml", GetEvents());
+    }
+    public IActionResult EventBuchen()
+    {
+        return View("Views/Home/EventBuchen.cshtml", GetEventDaten(1));
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
