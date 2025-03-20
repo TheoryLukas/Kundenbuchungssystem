a3f887330377cbaee9cab2925b4d1df05f80e9cc
Lukas Hannawald <lukas.hannawald@proton.me>
2025-03-20 20:31:03 +0100

    Hotfix: Add Filter To Events (active events)

---
+++ KBS_FunEvents_Web_2025/ViewModels/EventModelView.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KBS_FunEvents_Web_2025.ViewModels;
+
+public partial class EventModelView
+{
+    [Key]
+    public int EtEventId { get; set; } = 0;
+    public string EtBeschreibung { get; set; } = string.Empty;
+    public string EvEvVeranstalter { get; set; } = string.Empty;
+    public string EkEvKategorie { get; set; } = string.Empty;
+}
