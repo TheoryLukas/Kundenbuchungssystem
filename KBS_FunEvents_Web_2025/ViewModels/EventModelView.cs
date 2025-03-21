09680a32368e887359ad0b3fc37638b51352c32d
Lukas Hannawald <lukas.hannawald@proton.me>
2025-03-21 07:44:59 +0100

    fix: Eventbezeichung statt Beschreibung vewenden

--- KBS_FunEvents_Web_2025/ViewModels/EventModelView.cs
+++ KBS_FunEvents_Web_2025/ViewModels/EventModelView.cs
@@ -6,7 +6,7 @@ public partial class EventModelView
 {
     [Key]
     public int EtEventId { get; set; } = 0;
-    public string EtBeschreibung { get; set; } = string.Empty;
+    public string EtBezeichnung { get; set; } = string.Empty;
     public string EvEvVeranstalter { get; set; } = string.Empty;
     public string EkEvKategorie { get; set; } = string.Empty;
 }
